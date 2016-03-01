69e12b2b3771f994635b132f6c58abbb740f16c4
SirAwsm <200016@hkis.edu.hk>
2016-03-01 20:19:57 +0800

    changed link of form, removed some useless files

--- process.php
+++
@@ -1,8 +0,0 @@
-Thanks, <?php
-@extract($_POST);
-$sub="Form feedback";
-$name = stripslashes($name);
-$email = stripslashes($email);
-mail('200016@hkis.edu.hk',$sub,"$name $email","From: $name <admin@your-domain.com>");
-echo stripslashes($name);
-?> , we will drop you a line shortly.
